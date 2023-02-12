cdfac844072edb4f235eafc0cdeb37bfc1c8648c
thalestale <113582764+thalestale@users.noreply.github.com>
2023-02-12 13:33:17 -0300

    MusicPlayer

--- main/cenas/Testes/escada.gd
+++ main/cenas/Testes/escada.gd
@@ -4,6 +4,9 @@ var resource = load("res://cenas/Testes/dialogo.dialogue")
 var title : String = "abc"
 var playercogu : bool = false
 
+func _ready() -> void:
+	Musica.Tocar("LUNETA", 1.0)
+
 func _input(_event) -> void:
 	if Input.is_action_just_pressed("ui_accept") and playercogu:
 		set_process_input(false)
---
+++ main/core/Musica.gd
@@ -0,0 +1,36 @@
+extends Node
+
+var NodeMusica 
+var MusicaAnterior
+var MusicaAnteriorString
+
+func Tocar(musica : String, transicao : float) -> void:
+	#baixa o volume da musica que tava tocando antes
+	if MusicaAnterior == get_node("/root/Musica/Luneta"):
+		MusicaAnteriorString = "LUNETA"
+	else:
+		pass
+		
+	if musica == MusicaAnteriorString or MusicaAnteriorString == "":
+		pass
+	else:
+		NodeMusica = MusicaAnterior
+		DesligaMusicaAnterior(transicao)
+	
+	#associa o argumento ao node ----------------------------------------------
+	match musica:
+		"LUNETA":
+			NodeMusica = $Luneta
+		
+	#comeca a tocar a musica --------------------------------------------------
+	NodeMusica.volume_db = -80.0 #volume minimo antes de tocar
+	NodeMusica.play()
+	get_tree().create_tween().tween_property(NodeMusica, "volume_db", 0.0, transicao)
+	
+
+func DesligaMusicaAnterior(transicao : float) -> void:
+	if NodeMusica:
+		
+		await get_tree().create_tween().tween_property(NodeMusica, "volume_db", -80.0, transicao).fin
+		MusicaAnterior.playing = false
+	
